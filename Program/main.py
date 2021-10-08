61fcf6998368bbb18ea4ab87c6436a8bbf8f3eb5
Kamil Kowalewski <szamil24@gmail.com>
2021-10-08 18:11:22 +0200

    Init of python project

---
+++ Program/main.py
@@ -0,0 +1,46 @@
+import subprocess
+import sys
+from argparse import ArgumentParser, Namespace
+
+"""
+"""
+
+
+def main() -> None:
+    args = prepare_args()
+
+    display_finish()
+
+
+def prepare_args() -> Namespace:
+    arg_parser = ArgumentParser()
+
+    return arg_parser.parse_args()
+
+
+def check_types_check_style() -> None:
+    subprocess.call(["mypy", "."])
+    subprocess.call(["flake8", "."])
+
+
+def compile_to_pyc() -> None:
+    subprocess.call(["python", "-m", "compileall", "."])
+
+
+def check_if_exists_in_args(arg: str) -> bool:
+    return arg in sys.argv
+
+
+def display_finish() -> None:
+    print("------------------------------------------------------------------------")
+    print("FINISHED")
+    print("------------------------------------------------------------------------")
+
+
+if __name__ == "__main__":
+    if check_if_exists_in_args("-t"):
+        check_types_check_style()
+    elif check_if_exists_in_args("-b"):
+        compile_to_pyc()
+    else:
+        main()

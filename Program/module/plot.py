ce3504041cf88120f5c4c0a834050d6bd18523ef
Kamil Kowalewski <szamil24@gmail.com>
2021-10-21 13:54:25 +0200

    Small fix

--- Program/module/plot.py
+++ Program/module/plot.py
@@ -5,7 +5,7 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 
 from module.LabelNamesMapper import LabelNamesMapper
-from module.utils import prepare_filename
+from module.utils import create_directory, prepare_filename
 
 RESULTS_DIR = "saved_plots/"
 
@@ -23,6 +23,8 @@ def plot_charts(dataset: pd.DataFrame, save_data: bool) -> None:
     for name in LabelNamesMapper.get_non_numeric_column_names():
         dataset[name] = label_encoder.fit_transform(dataset[name])
 
+    create_directory(RESULTS_DIR)
+
     for params in PARAMS_SETUP:
         draw_plot(dataset, params[0], params[1], save_data)
 
@@ -31,6 +33,6 @@ def draw_plot(dataset: pd.DataFrame, x_axis_col_name: str,
               y_axis_col_name: str, save_data: bool) -> None:
     plt.plot(dataset[x_axis_col_name], dataset[y_axis_col_name])
     if save_data:
-        plt.savefig(RESULTS_DIR + prepare_filename(f"{x_axis_col_name}_{y_axis_col_name}"))
+        plt.savefig(RESULTS_DIR + prepare_filename(f"{x_axis_col_name}#{y_axis_col_name}"))
         plt.close()
     plt.show()

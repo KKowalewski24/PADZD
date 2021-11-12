680c4a3bafc2b7b1a48c76e5b3c9f1b2f356eb15
Kamil Kowalewski <szamil24@gmail.com>
2021-11-12 12:00:01 +0100

    Remove redundant fields from LabelNamesMapper.py

--- Program/module/LabelNamesMapper.py
+++ Program/module/LabelNamesMapper.py
@@ -1,29 +1,19 @@
-from typing import List
-
-
 class IdentifierLabels:
     ID = "CMPLNT_NUM"
 
 
 class DateTimeEventLabels:
-    EVENT_START_DATE = "CMPLNT_FR_DT"
-    EVENT_START_TIME = "CMPLNT_FR_TM"
     EVENT_START_TIMESTAMP = "CMPLNT_FR_TIMESTAMP"
-    EVENT_END_DATE = "CMPLNT_TO_DT"
-    EVENT_END_TIME = "CMPLNT_TO_TM"
     EVENT_END_TIMESTAMP = "CMPLNT_TO_TIMESTAMP"
 
 
 class DateTimeSubmissionLabels:
-    SUBMISSION_TO_POLICE_DATE = "RPT_DT"
     SUBMISSION_TO_POLICE_TIMESTAMP = "RPT_TIMESTAMP"
 
 
 class LawBreakingLabels:
     KEY_CODE = "KY_CD"
-    OFFENSE_DESCRIPTION = "OFNS_DESC"
     PD_CODE = "PD_CD"
-    PD_DESCRIPTION = "PD_DESC"
     LAW_BREAKING_LEVEL = "LAW_CAT_CD"
 
 
@@ -39,19 +29,8 @@ class EventSurroundingsLabels:
 class EventLocationLabels:
     PRECINCT_CODE = "ADDR_PCT_CD"
     BOROUGH_NAME = "BORO_NM"
-    JURISDICTION_DESCRIPTION = "JURIS_DESC"
-    JURISDICTION_CODE = "JURISDICTION_CODE"
-    PARK_NAME = "PARKS_NM"
-    NYC_HOUSING_DEVELOPMENT = "HADEVELOPT"
-    DEVELOPMENT_LEVEl_CODE = "HOUSING_PSA"
-    NYC_X_COORDINATE = "X_COORD_CD"
-    NYC_Y_COORDINATE = "Y_COORD_CD"
-    TRANSIT_DISTRICT_CODE = "TRANSIT_DISTRICT"
     LATITUDE = "Latitude"
     LONGITUDE = "Longitude"
-    LATITUDE_LONGITUDE = "Lat_Lon"
-    PATROL_DISTRICT_NAME = "PATROL_BORO"
-    TRANSIT_STATION_NAME = "STATION_NAME"
 
 
 class SuspectLabels:
@@ -76,11 +55,3 @@ class LabelNamesMapper:
     event_location: EventLocationLabels = EventLocationLabels
     suspect: SuspectLabels = SuspectLabels
     victim: VictimLabels = VictimLabels
-
-
-    @staticmethod
-    def get_non_numeric_column_names() -> List[str]:
-        # TODO ADD ALL FIELDS THAT ARE NOT NUMERIC
-        return [
-
-        ]

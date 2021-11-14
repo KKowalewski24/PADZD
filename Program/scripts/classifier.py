4107cc5f99df305e2b0f183ff08ed1bcb0570511
sgruda <szymon.gruda98@gmail.com>
2021-11-14 15:53:42 +0100

    First executing of forest

--- Program/scripts/classifier.py
+++ Program/scripts/classifier.py
@@ -6,17 +6,24 @@ import sys
 from argparse import ArgumentParser, Namespace
 from datetime import datetime
 from typing import Dict, List, Tuple
+from module.utils import display_finish, run_main
 
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+import calendar
 
 import pandas as pd
+
+from module.label_names_mapper import *
 
 """
 How to run:
     python classifier.py -f ../data/NYPD_Data_Preprocessed-1K.csv
+    python classifier.py module/label_names_mapper.py -f ../data/NYPD_Data_Preprocessed-1K.csv
     python classifier.py -f Pulpit/data/NYPD_Data_Preprocessed-1K.csv
     python E:\OneDrive\OneDrive - Politechnika Łódzka\2stopien\Semestr 2\PADZD\Program\scriptsclassifier.py -f Pulpit/data/NYPD_Data_Preprocessed-1K.csv
 
@@ -35,14 +42,16 @@ logging.basicConfig(
 
 
 def main() -> None:
-    args = prepare_args()
-    filepath = args.filepath
+    # args = prepare_args()
+    # filepath = args.filepath
+    filepath = "../data/NYPD_Data_Preprocessed-1K.csv"
     create_directory(RESULTS_DIR)
 
     display_and_log("Loading data...")
     df: pd.DataFrame = pd.read_csv(filepath)
     display_and_log(f"Size of loaded data: {len(df.index)}")
 
+    df = preprocess_data(df)
     decision_tree_classification(df)
 
     display_and_log("Saving data to file")
@@ -51,7 +60,12 @@ def main() -> None:
 
 
 def decision_tree_classification(data_set: pd.DataFrame) -> None:
-    X_train, X_test, y_train, y_test = data_set.spl
+    train, test = train_test_split(data_set, test_size=0.2)
+
+    y_train = train[LawBreakingLabels.KEY_CODE]
+    x_train = train.drop(columns=[LawBreakingLabels.KEY_CODE])
+    y_test = test[LawBreakingLabels.KEY_CODE]
+    x_test = test.drop(columns=[LawBreakingLabels.KEY_CODE])
 
     min_samples_leaf_range = [
        10, 100, 1000, 10000
@@ -60,27 +74,109 @@ def decision_tree_classification(data_set: pd.DataFrame) -> None:
     n_estimators_range = [100, 1000, 10000]
 
 
-    train_acc_history = []
-    test_acc_history = []
-    for n_estimators in n_estimators_range:
-        forest = RandomForestClassifier(random_state=47,
+    forest = RandomForestClassifier(random_state=47,
                                         n_jobs=-1,
-                                        n_estimators=n_estimators,
+                                        n_estimators=100,
                                         max_depth=100,
                                         min_samples_leaf=10
                                         )
-        forest.fit(X_train, y_train)
-        train_acc_history.append(forest.score(X_train, y_train))
-        test_acc_history.append(forest.score(X_test, y_test))
-        print("n_estimators:", n_estimators, "\ttrain_acc:",
-              train_acc_history[-1], "\ttest_acc:", test_acc_history[-1])
-    # _plot_accuracy(413, train_acc_history, test_acc_history, "n_estimators",
-    #                n_estimators_range)
-    best_acc = np.max(test_acc_history)
+    forest.fit(x_train, y_train)
+    train_acc_history = []
+    test_acc_history = []
+    train_acc_history.append(forest.score(x_train, y_train))
+    test_acc_history.append(forest.score(x_test, y_test))
 
-    print("best accuracy:", best_acc)
-    plt.show()
+    print("n_estimators:", 100, "\ttrain_acc:",
+          train_acc_history[-1], "\ttest_acc:", test_acc_history[-1])
 
+    # train_acc_history = []
+    # test_acc_history = []
+    # for n_estimators in n_estimators_range:
+    #     forest = RandomForestClassifier(random_state=47,
+    #                                     n_jobs=-1,
+    #                                     n_estimators=n_estimators,
+    #                                     max_depth=100,
+    #                                     min_samples_leaf=10
+    #                                     )
+    #     forest.fit(X_train, y_train)
+    #     train_acc_history.append(forest.score(X_train, y_train))
+    #     test_acc_history.append(forest.score(X_test, y_test))
+    #     print("n_estimators:", n_estimators, "\ttrain_acc:",
+    #           train_acc_history[-1], "\ttest_acc:", test_acc_history[-1])
+    # # _plot_accuracy(413, train_acc_history, test_acc_history, "n_estimators",
+    # #                n_estimators_range)
+    # best_acc = np.max(test_acc_history)
+    #
+    # print("best accuracy:", best_acc)
+    # plt.show()
+
+
+def preprocess_data(data: pd.DataFrame) -> pd.DataFrame:
+    data = data.drop(columns=[IdentifierLabels.ID,
+                              LawBreakingLabels.PD_CODE,
+                              EventLocationLabels.PRECINCT_CODE,
+                              EventLocationLabels.BOROUGH_NAME,
+                              EventLocationLabels.LATITUDE,
+                              EventLocationLabels.LONGITUDE,
+                              SuspectLabels.SUSPECT_AGE_GROUP,
+                              VictimLabels.VICTIM_AGE_GROUP,
+                              DateTimeSubmissionLabels.SUBMISSION_TO_POLICE_TIMESTAMP,
+                              DateTimeEventLabels.EVENT_END_TIMESTAMP
+                              ])
+
+    data = remove_na(data)
+    data = extract_hour_and_day(data)
+    data = transform_labels(data)
+    return data
+
+
+def remove_na(data: pd.DataFrame) -> pd.DataFrame:
+    columns = [
+        DateTimeEventLabels.EVENT_START_TIMESTAMP,
+        # DateTimeEventLabels.EVENT_END_TIMESTAMP,
+        # DateTimeSubmissionLabels.SUBMISSION_TO_POLICE_TIMESTAMP,
+        LawBreakingLabels.KEY_CODE,
+        LawBreakingLabels.LAW_BREAKING_LEVEL,
+        EventStatusLabels.EVENT_STATUS,
+        EventSurroundingsLabels.PLACE_TYPE,
+        EventSurroundingsLabels.PLACE_TYPE_POSITION,
+        # SuspectLabels.SUSPECT_AGE_GROUP,
+        SuspectLabels.SUSPECT_RACE,
+        SuspectLabels.SUSPECT_SEX,
+        # VictimLabels.VICTIM_AGE_GROUP,
+        VictimLabels.VICTIM_RACE,
+        VictimLabels.VICTIM_SEX
+    ]
+    for column in columns:
+        data = data[data[column].notna()]
+    return data
+
+
+def transform_labels(data: pd.DataFrame) -> pd.DataFrame:
+    categorical_columns = [
+        EventStatusLabels.EVENT_STATUS,
+        LawBreakingLabels.LAW_BREAKING_LEVEL,
+        EventSurroundingsLabels.PLACE_TYPE,
+        EventSurroundingsLabels.PLACE_TYPE_POSITION,
+        VictimLabels.VICTIM_RACE,
+        VictimLabels.VICTIM_SEX,
+        SuspectLabels.SUSPECT_RACE,
+        SuspectLabels.SUSPECT_SEX,
+        'DayOfWeek'
+    ]
+    encoder = LabelEncoder()
+    for column in categorical_columns:
+        data[column] = encoder.fit_transform(data[column])
+    return data
+
+
+def extract_hour_and_day(data: pd.DataFrame) -> pd.DataFrame:
+    data['DayOfWeek'] = pd.to_datetime(data[DateTimeEventLabels.EVENT_START_TIMESTAMP])\
+        .map(lambda date: calendar.day_name[date.weekday()])
+    data['Hour'] = pd.to_datetime(data[DateTimeEventLabels.EVENT_START_TIMESTAMP]) \
+        .map(lambda date: date.hour)
+    data = data.drop(columns=[DateTimeEventLabels.EVENT_START_TIMESTAMP])
+    return data
 
 def prepare_args() -> Namespace:
     arg_parser = ArgumentParser()
@@ -114,3 +210,6 @@ def display_finish() -> None:
 def display_and_log(text: str) -> None:
     print(text)
     logging.info(text)
+
+if __name__ == "__main__":
+    main()
